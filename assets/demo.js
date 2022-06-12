31916112b5f1cf2d28a60aeff579b7ed62e7b36d
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-12 17:25:48 +0300

    added a scroll up button

--- assets/demo.js
+++ assets/demo.js
@@ -1,3 +1,20 @@
+mybutton = document.getElementById("topbtn");
+
+window.onscroll =()=> {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  }
+
+
+
 function validateEmail(inputText)
 {
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
